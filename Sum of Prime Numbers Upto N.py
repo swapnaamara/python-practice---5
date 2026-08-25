3bd53afcaa85dbc3ff4855de37fecebcb9e5e6ee
swapnaamara <swapnaamara59@gmail.com>
2026-08-25 22:13:54 +0530

    Refactor prime number summation code

--- Sum of Prime Numbers Upto N.py
+++ Sum of Prime Numbers Upto N.py
@@ -3,8 +3,10 @@ total = 0
 
 for num in range(2, n+1):
     is_prime = True
+    
     for i in range(2, num):
         if num % i == 0:
+            
             is_prime = False
             break
     if is_prime:
