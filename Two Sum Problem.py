ce249a904db9499b430d049e85780fcd6e8e4ab9
swapnaamara <swapnaamara59@gmail.com>
2026-08-25 22:14:53 +0530

    Refactor input handling in Two Sum Problem

--- Two Sum Problem.py
+++ Two Sum Problem.py
@@ -1,13 +1,16 @@
 n = int(input("Enter size: "))
 arr = []
+
 for i in range(n):
     arr.append(int(input(f"Enter value {i+1}: ")))
 
 target = int(input("Enter target: "))
 
 found = False
+
 for i in range(n):
     for j in range(i+1, n):
+        
         if arr[i] + arr[j] == target:
             print(i, j)
             found = True
