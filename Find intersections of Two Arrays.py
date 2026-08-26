ff4f369c1c05ec6b40445687b95ca5b8e39fa170
swapnaamara <swapnaamara59@gmail.com>
2026-08-26 15:09:31 +0530

    Optimize intersection search with break statement
    Added break statement to stop inner loop after finding an intersection.

--- Find intersections of Two Arrays.py
+++ Find intersections of Two Arrays.py
@@ -1,14 +1,17 @@
 n1 = int(input("Enter size of first array: "))
 a = []
+
 for i in range(n1):
     a.append(int(input(f"Enter value {i+1} for first array: ")))
 
 n2 = int(input("Enter size of second array: "))
 b = []
+
 for i in range(n2):
     b.append(int(input(f"Enter value {i+1} for second array: ")))
 
 print("Intersection:", end=" ")
+
 for i in range(n1):
     for j in range(n2):
         if a[i] == b[j]:
