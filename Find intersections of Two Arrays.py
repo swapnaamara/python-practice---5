ce37dd7f8b3a77aeefc6dd853704fe43c90d88ed
swapnaamara <swapnaamara59@gmail.com>
2026-08-26 15:05:49 +0530

    Add files via upload

---
+++ Find intersections of Two Arrays.py
@@ -0,0 +1,16 @@
+n1 = int(input("Enter size of first array: "))
+a = []
+for i in range(n1):
+    a.append(int(input(f"Enter value {i+1} for first array: ")))
+
+n2 = int(input("Enter size of second array: "))
+b = []
+for i in range(n2):
+    b.append(int(input(f"Enter value {i+1} for second array: ")))
+
+print("Intersection:", end=" ")
+for i in range(n1):
+    for j in range(n2):
+        if a[i] == b[j]:
+            print(a[i], end=" ")
+            break
